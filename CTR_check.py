818f017dee7fabf35bab8f74bef7ecae2c87bb83
jpolton <jpolton@gmail.com>
2021-11-04 16:48:29 +0000

    Update with external shoothill api

--- CTR_check.py
+++ CTR_check.py
@@ -10,7 +10,7 @@ Created on Sat Jan 30 15:55:23 2021
 Check the gauges at Chester.
 Plot Chester weir height (above AND below weir) + flow
 Plot Gladstone and Ironbridge levels.
-
+Uses shoothill_api package to augment COAsT
 
 This tutorial uses data from the Shoothill API. This does require a key to be
 setup. It is assumed that the key is privately stored in
@@ -22,6 +22,24 @@ To discover the StationId for a particular measurement site check the
  integer id in the url or its twitter page having identified it via
   https://www.gaugemap.co.uk/#!Map
 E.g  Liverpool (Gladstone Dock stationId="13482", which is read by default.
+
+Env: workshop_env with coast and requests installed,
+E.g.
+## Create an environment with coast installed
+yes | conda env remove --name workshop_env
+yes | conda create --name workshop_env python=3.8
+conda activate workshop_env
+yes | conda install -c bodc coast=1.2.7
+# enforce the GSW package number (something fishy with the build process bumped up this version number)
+yes | conda install -c conda-forge gsw=3.3.1
+# install cartopy, not part of coast package
+yes | conda install -c conda-forge cartopy=0.20.1
+
+## install request for shoothill server requests
+conda install requests
+
+Usage:
+python CTR_check.py
 '''
 
 # Begin by importing coast and other packages
@@ -29,13 +47,16 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import xarray as xr
+
+from shoothill_api.shoothill_api import GAUGE
 
 
-import os,sys
-coastdir = os.path.dirname('/Users/jeff/GitHub/COAsT/coast')
-sys.path.insert(0, coastdir)
-import coast
 
+
+
+
+################################################################################
 #%%  plot functions
 def line_plot(ax, time, y, color, size, label=None ):
     #ax1.scatter(liv.dataset.time, liv.dataset.sea_level, color='k', s=1, label=liv.dataset.site_name)
@@ -47,108 +68,129 @@ def scatter_plot(ax, time, y, color, size, label=None ):
     ax.scatter(time, y, color=color, s=size, label=label)
     return ax
 #%%
-ndays = 1
 
-date_end = np.datetime64('now') 
-date_start = date_end - np.timedelta64(ndays,'D')
 
-## Christoph 22 Jan 21
-date_end = np.datetime64('2021-01-28') 
-ndays = 10
-date_start = date_end - np.timedelta64(ndays,'D')
-
-## 2021
-date_end = np.datetime64('now') 
-date_start = np.datetime64('2021-01-01')
-
-#%% Load data
-
-# Load in data from the Shoothill API. Gladstone dock is loaded by default
-liv = coast.TIDEGAUGE()
-liv.dataset = liv.read_shoothill_to_xarray(date_start=date_start, date_end=date_end)
-#liv.plot_timeseries()
-
-ctrf = coast.TIDEGAUGE()
-ctrf.dataset = ctrf.read_shoothill_to_xarray(stationId="7899" ,date_start=date_start, date_end=date_end, dataType=15)
-#ctrf.plot_timeseries()
-
-ctr = coast.TIDEGAUGE()
-ctr.dataset = ctr.read_shoothill_to_xarray(stationId="7899" ,date_start=date_start, date_end=date_end)
-#ctr.plot_timeseries()
-
-ctr2 = coast.TIDEGAUGE()
-ctr2.dataset = ctr.read_shoothill_to_xarray(stationId="7900" ,date_start=date_start, date_end=date_end)
-#ctr2.plot_timeseries()
-
-iron = coast.TIDEGAUGE()
-iron.dataset = ctr.read_shoothill_to_xarray(stationId="968" ,date_start=date_start, date_end=date_end)
-#iron.plot_timeseries()
-
-#farn = coast.TIDEGAUGE()
-#farn.dataset = ctr.read_shoothill_to_xarray(stationId="972" ,date_start=date_start, date_end=date_end)
-#farn.plot_timeseries()
+################################################################################
+################################################################################
+#%% Main Routine
+################################################################################
+################################################################################
+if __name__ == "__main__":
 
 
 
 
-#%% Plot data
-
-# Top: Gladstone + Ironbridge
-# Lower: CTR height + flow
-
-line_flag = True
-today_only_flag = True
-
-plt.close('all')
-fig, (ax1, ax2) = plt.subplots(2, sharex=True)
-
-## Only get tides over the weir with 8.75m at Liverpool
-fig.suptitle('Dee River heights and flow')
-#ax1.scatter(liv.dataset.time, liv.dataset.sea_level, color='k', s=1, label=liv.dataset.site_name)
-ax1 = scatter_plot(ax1, liv.dataset.time, liv.dataset.sea_level, 'k', 1, liv.dataset.site_name)
-if line_flag:
-    ax1 = line_plot(ax1, liv.dataset.time, liv.dataset.sea_level, 'k', 1)
-    
-ax1.plot( [date_start - np.timedelta64(1,'D'), date_end], [8.75,8.75], 'k--')
-ax1b = ax1.twinx()
-ax1b = scatter_plot(ax1b, iron.dataset.time, iron.dataset.sea_level, 'b', 1, iron.dataset.site_name)
-if line_flag:
-    ax1b = line_plot(ax1b, iron.dataset.time, iron.dataset.sea_level, 'b', 1)
-
-ax1.set_ylabel('water level (m)', color='k')
-ax1b.set_ylabel('water level (m)', color='b')
-for tl in ax1b.get_yticklabels():
-    tl.set_color('b')
-    
-ax1.legend(markerscale=6)
-ax1b.legend(markerscale=6)
 
 
-ax2 = scatter_plot(ax2, ctr.dataset.time, ctr.dataset.sea_level, 'k', 1, "Chester, above weir")
-if line_flag:
-    ax2 = line_plot(ax2, ctr.dataset.time, ctr.dataset.sea_level, 'k', 1)
-ax2 = scatter_plot(ax2, ctr2.dataset.time, ctr2.dataset.sea_level, 'b', 1, "Chester, below weir")
-if line_flag:
-    ax2 = line_plot(ax2, ctr2.dataset.time, ctr2.dataset.sea_level, 'b', 1)
-ax2b = ax2.twinx()
-ax2b = scatter_plot(ax2b, ctrf.dataset.time, ctrf.dataset.sea_level, 'g', 1, ctrf.dataset.site_name)
-if line_flag:
-    ax2b = line_plot(ax2b, ctrf.dataset.time, ctrf.dataset.sea_level, 'g', 1)
-ax2b.set_ylabel('flow rate (m3/s)', color='g')
-for tl in ax2b.get_yticklabels():
-    tl.set_color('g')
-ax2.set_ylabel('water level (m)')
+    ndays = 2
 
-myFmt = mdates.DateFormatter('%H:%M') #('%d-%a')
-ax2.xaxis.set_major_formatter(myFmt)
-#ax2.set_xlabel( date_start.astype(datetime.datetime).strftime('%d%b%y') + \
-#               '-' + date_end.astype(datetime.datetime).strftime('%d%b%y') )
-ax2.set_xlabel(date_end.astype(datetime.datetime).strftime('%d%b%y'))
-# Add empty data to ax1 to get "green flow data" in the legend
-ax2 = scatter_plot(ax2, [], [], 'g', 1, "Flow, above weir")
+    date_end = np.datetime64('now')
+    date_start = date_end - np.timedelta64(ndays,'D')
 
-# plot the legend
-ax2.legend(markerscale=6)
+    ## Christoph 22 Jan 21
+    date_end = np.datetime64('2021-01-28')
+    ndays = 10
+    date_start = date_end - np.timedelta64(ndays,'D')
 
-plt.savefig('Chester_river_levels.png')
+    ## 2021
+    date_end = np.datetime64('now')
+    date_start = np.datetime64('2021-01-01')
 
+    ## 24 hrs
+    date_end = np.datetime64('now')
+    date_start = np.datetime64('now') - np.timedelta64(72,'h')
+
+
+
+    #%% Load data
+
+    # Load in data from the Shoothill API. Gladstone dock is loaded by default
+    #liv = coast.Tidegauge()
+    liv = GAUGE()
+    liv.dataset = liv.read_shoothill_to_xarray(date_start=date_start, date_end=date_end)
+    #liv.plot_timeseries()
+
+    ctrf = GAUGE()
+    ctrf.dataset = ctrf.read_shoothill_to_xarray(station_id="7899" ,date_start=date_start, date_end=date_end, dataType=15)
+    #ctrf.plot_timeseries()
+
+    ctr = GAUGE()
+    ctr.dataset = ctr.read_shoothill_to_xarray(station_id="7899" ,date_start=date_start, date_end=date_end)
+    #ctr.plot_timeseries()
+
+    ctr2 = GAUGE()
+    ctr2.dataset = ctr.read_shoothill_to_xarray(station_id="7900" ,date_start=date_start, date_end=date_end)
+    #ctr2.plot_timeseries()
+
+    iron = GAUGE()
+    iron.dataset = ctr.read_shoothill_to_xarray(station_id="968" ,date_start=date_start, date_end=date_end)
+    #iron.plot_timeseries()
+
+    #farn = GAUGE()
+    #farn.dataset = ctr.read_shoothill_to_xarray(station_id="972" ,date_start=date_start, date_end=date_end)
+    #farn.plot_timeseries()
+
+
+
+
+    #%% Plot data
+
+    # Top: Gladstone + Ironbridge
+    # Lower: CTR height + flow
+
+    line_flag = True
+    today_only_flag = True
+
+    plt.close('all')
+    fig, (ax1, ax2) = plt.subplots(2, sharex=True)
+
+    ## Only get tides over the weir with 8.75m at Liverpool
+    fig.suptitle('Dee River heights and flow')
+    #ax1.scatter(liv.dataset.time, liv.dataset.sea_level, color='k', s=1, label=liv.dataset.site_name)
+    ax1 = scatter_plot(ax1, liv.dataset.time, liv.dataset.sea_level, 'k', 1, liv.dataset.site_name)
+    if line_flag:
+        ax1 = line_plot(ax1, liv.dataset.time, liv.dataset.sea_level, 'k', 1)
+
+    ax1.plot( [date_start - np.timedelta64(1,'D'), date_end], [8.75,8.75], 'k--')
+    ax1b = ax1.twinx()
+    ax1b = scatter_plot(ax1b, iron.dataset.time, iron.dataset.sea_level, 'b', 1, iron.dataset.site_name)
+    if line_flag:
+        ax1b = line_plot(ax1b, iron.dataset.time, iron.dataset.sea_level, 'b', 1)
+
+    ax1.set_ylabel('water level (m)', color='k')
+    ax1b.set_ylabel('water level (m)', color='b')
+    for tl in ax1b.get_yticklabels():
+        tl.set_color('b')
+
+    ax1.legend(markerscale=6)
+    ax1b.legend(markerscale=6)
+
+
+    ax2 = scatter_plot(ax2, ctr.dataset.time, ctr.dataset.sea_level, 'k', 1, "Chester, above weir")
+    if line_flag:
+        ax2 = line_plot(ax2, ctr.dataset.time, ctr.dataset.sea_level, 'k', 1)
+    ax2 = scatter_plot(ax2, ctr2.dataset.time, ctr2.dataset.sea_level, 'b', 1, "Chester, below weir")
+    if line_flag:
+        ax2 = line_plot(ax2, ctr2.dataset.time, ctr2.dataset.sea_level, 'b', 1)
+    ax2b = ax2.twinx()
+    ax2b = scatter_plot(ax2b, ctrf.dataset.time, ctrf.dataset.sea_level, 'g', 1, ctrf.dataset.site_name)
+    if line_flag:
+        ax2b = line_plot(ax2b, ctrf.dataset.time, ctrf.dataset.sea_level, 'g', 1)
+    ax2b.set_ylabel('flow rate (m3/s)', color='g')
+    for tl in ax2b.get_yticklabels():
+        tl.set_color('g')
+    ax2.set_ylabel('water level (m)')
+
+    #myFmt = mdates.DateFormatter('%H:%M') #('%d-%a')
+    #ax2.xaxis.set_major_formatter(myFmt)
+
+    #ax2.set_xlabel( date_start.astype(datetime.datetime).strftime('%d%b%y') + \
+    #               '-' + date_end.astype(datetime.datetime).strftime('%d%b%y') )
+    ax2.set_xlabel(date_end.astype(datetime.datetime).strftime('%d%b%y'))
+    # Add empty data to ax1 to get "green flow data" in the legend
+    ax2 = scatter_plot(ax2, [], [], 'g', 1, "Flow, above weir")
+
+    # plot the legend
+    ax2.legend(markerscale=6, loc='lower left')
+
+    plt.savefig('Chester_river_levels.png')
---
+++ shoothill_api/shoothill_api.py
@@ -0,0 +1,219 @@
+"""
+COAsT add on with shoothill api wrapper
+
+Created on 2021-11-04
+@author: jelt
+
+This package augements the COAsT package acting as a wrapper for the Shoothill
+API. This does require a key to be setup. It is assumed that the key is
+privately stored in
+ config_keys.py
+
+The shoothill API aggregates data across the country for a variety of instruments but,
+ requiring a key, is trickier to set up than the EA API.
+
+To discover the StationId for a particular measurement site check the
+ integer id in the url or its twitter page having identified it via
+  https://www.gaugemap.co.uk/#!Map
+E.g  Liverpool (Gladstone Dock stationId="13482", which is read by default.
+
+Conda environment:
+    coast + requests,
+    (E.g. workshop_env w/ requests)
+
+    ### Build python environment:
+    ## Create an environment with coast installed
+    yes | conda env remove --name workshop_env
+    yes | conda create --name workshop_env python=3.8
+    conda activate workshop_env
+    yes | conda install -c bodc coast=1.2.7
+    # enforce the GSW package number (something fishy with the build process bumped up this version number)
+    yes | conda install -c conda-forge gsw=3.3.1
+    # install cartopy, not part of coast package
+    yes | conda install -c conda-forge cartopy=0.20.1
+
+    ## install request for shoothill server requests
+    conda install requests
+
+Example usage:
+    from shoothill_api.shoothill_api import GAUGE
+    liv = GAUGE()
+    liv.dataset = liv.read_shoothill_to_xarray(ndays=5)
+    liv.plot_timeseries()
+
+
+To do:
+    * logging doesn't work
+"""
+
+import coast
+import datetime
+import numpy as np
+import xarray as xr
+
+import logging
+logging.basicConfig(filename='shoothill2.log', filemode='w+')
+logging.getLogger().setLevel(logging.INFO)
+
+
+#%% ################################################################################
+class GAUGE(coast.Tidegauge):
+    """ Inherit from COAsT. Add new methods """
+
+    def __init__(self, ndays: int=5, startday: datetime=None, endday: datetime=None, station_id="7708"):
+        try:
+            import config_keys # Load secret keys
+        except:
+            logging.info('Need a Shoothil API Key. Use e.g. create_shoothill_key() having obtained a public key')
+            print('Need a Shoothil API Key. Use e.g. create_shoothill_key() having obtained a public key')
+
+        #self.SessionHeaderId=config_keys.SHOOTHILL_KEY #'4b6...snip...a5ea'
+        self.ndays=ndays
+        self.startday=startday
+        self.endday=endday
+        self.station_id=station_id # Shoothill id
+
+        #self.dataset = self.read_shoothill_to_xarray(station_id="13482") # Liverpool
+
+
+        pass
+
+
+############ shoothill gauge methods ##############################################
+    @classmethod
+    def read_shoothill_to_xarray(cls,
+                                ndays: int=5,
+                                date_start: np.datetime64=None,
+                                date_end: np.datetime64=None,
+                                station_id="7708",
+                                dataType=3):
+        """
+        load gauge data via shoothill API
+        Either loads last ndays, or from date_start:date_end
+
+        This reqires an API key that is obtained by emailing shoothill.
+        They provide a public key. Then SHOOTHILL_KEY can be generated using
+        SHOOTHILL_KEY = create_shoothill_key(SHOOTHILL_PublicApiKey)
+
+        To discover the station_id for a particular measurement site check the
+         integer id in the url or its twitter page having identified it via
+          https://www.gaugemap.co.uk/#!Map
+         E.g  Liverpool (Gladstone Dock station_id="13482".
+        Liverpool, or station_id="13482", is assumed by default.
+
+        INPUTS:
+            ndays : int
+            date_start : datetime. UTC format string "yyyy-MM-ddThh:mm:ssZ" E.g 2020-01-05T08:20:01.5011423+00:00
+            date_end : datetime
+            station_id : str (station id)
+            dataType: int (3 level, 15 flow)
+        OUTPUT:
+            sea_level, time : xr.Dataset
+        """
+        import requests,json
+
+        try:
+            import config_keys # Load secret keys
+        except:
+            logging.info('Need a Shoothil API Key. Use e.g. create_shoothill_key(SHOOTHILL_PublicApiKey) having obtained a public key')
+            print('Expected a config_keys.py file of the form:')
+            print('')
+            print('# API keys excluded from github repo')
+            print('SHOOTHILL_KEY = "4b6...5ea"')
+            print('SHOOTHILL_PublicApiKey = "9a1...414"')
+
+        cls.SessionHeaderId=config_keys.SHOOTHILL_KEY #'4b6...snip...a5ea'
+        cls.ndays=ndays
+        cls.date_start=date_start
+        cls.date_end=date_end
+        cls.station_id=station_id # Shoothill id
+        cls.dataType=dataType
+
+        logging.info("load gauge")
+
+        if cls.station_id == "7708":
+            id_ref = "Gladston Dock"
+        elif cls.station_id == "7899":
+            id_ref = "Chester weir"
+        elif cls.station_id == "972":
+            id_ref = "Farndon"
+        elif cls.station_id == "968":
+            id_ref = "Ironbridge (Dee)"
+        else:
+            id_ref = "No label"
+            logging.debug(f"Not ready for that station id. {cls.station_id}")
+
+        headers = {'content-type': 'application/json', 'SessionHeaderId': cls.SessionHeaderId}
+
+        ## Construct station info API request
+        # Obtain and process header information
+        logging.info("load station info")
+        print("load station info")
+        htmlcall_station_id = 'http://riverlevelsapi.shoothill.com/TimeSeries/GetTimeSeriesStationById/?stationId='
+        url  = htmlcall_station_id+str(station_id)
+        try:
+            request_raw = requests.get(url, headers=headers)
+            header_dict = json.loads(request_raw.content)
+            logging.debug(f"{header_dict}")
+        except ValueError:
+            print(f"Failed request for station {cls.station_id}")
+            return
+
+        # Assign expected header_dict information
+        try: # header_dict['latitude'] and header_dict['longitude'] are present
+            header_dict['site_name'] = header_dict['name']
+            #header_dict['latitude'] = header_dict['items']['lat']
+            #header_dict['longitude'] = header_dict['items']['long']
+        except:
+            logging.info(f"possible missing some header info: site_name,latitude,longitude")
+
+        ## Construct data API request
+        if (cls.date_start == None) & (cls.date_end == None):
+            logging.info(f"GETting ndays= {cls.ndays} of data")
+
+            htmlcall_station_id = 'http://riverlevelsapi.shoothill.com/TimeSeries/GetTimeSeriesRecentDatapoints/?stationId='
+            url  = htmlcall_station_id+str(cls.station_id)+'&dataType='+str(int(cls.dataType))+'&numberDays='+str(int(cls.ndays))
+        else:
+            # Check date_start and date_end are timetime objects
+            if (type(cls.date_start) is np.datetime64) & (type(cls.date_end) is np.datetime64):
+                logging.info(f"GETting data from {cls.date_start} to {cls.date_end}")
+                startTime = cls.date_start.item().strftime('%Y-%m-%dT%H:%M:%SZ')
+                endTime = cls.date_end.item().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+                htmlcall_station_id = 'http://riverlevelsapi.shoothill.com/TimeSeries/GetTimeSeriesDatapointsDateTime/?stationId='
+                url   = htmlcall_station_id+str(cls.station_id)+'&dataType='+str(int(cls.dataType))+'&endTime='+endTime+'&startTime='+startTime
+
+            else:
+                logging.debug('Expecting date_start and date_end as datetime objects')
+
+        ## Get the data
+        request_raw = requests.get(url, headers=headers)
+        request = json.loads(request_raw.content)
+        logging.debug(f"Shoothil API request: {request_raw.text}")
+
+        # Check the output
+        #logging.info(f"Gauge id is {request['gauge']['geoEntityId']}")
+        #logging.info(f"timestamp and value of the zero index is {[ str(request['values'][0]['time']), request['values'][0]['value'] ]}")
+
+        #print(request)
+        ## Process header information
+        #header_dict = request['gauge']
+        #header_dict['site_name'] = id_ref
+
+        ## Process timeseries data
+        dataset = xr.Dataset()
+        time = []
+        sea_level = []
+        nvals = len(request['values'])
+        time = np.array([np.datetime64(request['values'][i]['time']) for i in range(nvals)])
+        sea_level = np.array([request['values'][i]['value'] for i in range(nvals)])
+
+        ## Assign arrays to Dataset
+        dataset['sea_level'] = xr.DataArray(sea_level, dims=['time'])
+        dataset = dataset.assign_coords(time = ('time', time))
+        dataset.attrs = header_dict
+        #logging.debug(f"Shoothil API request headers: {header_dict}")
+        #logging.debug(f"Shoothil API request 1st time: {time[0]} and value: {sea_level[0]}")
+
+        # Assign local dataset to object-scope dataset
+        return dataset
